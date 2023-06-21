bcf12923f7561a9f29b8d27aea51c16a82c1056f
Darlington Obioha <epitome011@yahoo.com>
2023-06-21 07:32:04 -0700

    AirBnB_clone_v2

--- models/user.py
+++ models/user.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-
-"""This file defines the UserModel class
-It inherits from the BaseModel
-"""
-
+"""Defines the User class."""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """The User Model"""
+class User(BaseModel, Base):
+    """Represents a user for a MySQL database.
 
-    # Attributes
-    email: str = ""
-    password: str = ""
-    first_name: str = ""
-    last_name: str = ""
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The User-Place relationship.
+        reviews (sqlalchemy relationship): The User-Review relationship.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
